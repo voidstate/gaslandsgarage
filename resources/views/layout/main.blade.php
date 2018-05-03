b3d6038192bf5ec8a31bcf9bc29c198976af1535
Fergus Hadley <fhadley@serif.net>
2018-05-03 11:48:44 +0100

    Prevent pinch/zoom

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
 	<title>The Workshop &middot; Gaslands Garage</title>
