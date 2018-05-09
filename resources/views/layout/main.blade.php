eebd267e81395651c3b6c104a0121791122a613f
Fergus Hadley <fhadley@serif.net>
2018-05-09 12:49:35 +0100

    Favicons and social images

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
 	<title>The Workshop &middot; Gaslands Garage</title>
 
-	<link rel="icon" type="image/png" href="/images/favicon.png">
+	<!-- Favicon -->
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0b5156">
+	<meta name="msapplication-TileColor" content="#00aba9">
+	<meta name="theme-color" content="#0b5156">
+
+	<!-- Social -->
+	<meta property="og:image:width" content="279">
+	<meta property="og:image:height" content="279">
+	<meta property="og:description" content="Earth's Best Team and Vehicle Builder for the Gaslands Miniatures Game">
+	<meta property="og:title" content="Gaslands Garage">
+	<meta property="og:url" content="https://gaslandsgarage.co.uk">
+	<meta property="og:image" content="https://gaslandsgarage.co.uk/images/social/og-image.jpg">
 
 	<!-- Site CSS -->
 	<link rel="stylesheet" href="/css/app.css">
